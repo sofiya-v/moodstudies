69b112fef469aa71c2579c714f32b49802227f17
Sofiya Volobuyeva <sofiyavolobuyeva@gmail.com>
2026-08-05 20:11:31 -0400

    whole buncha things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    fonts: [
+        {
+            provider: fontProviders.google(),
+            name: 'Syne Mono',
+            cssVariable: '--font-syne-mono',
+        },
+        {
+            provider: fontProviders.google(),
+            name: 'Silkscreen',
+            cssVariable: '--font-silkscreen',
+        },
+
+    ],
+});
+
+
