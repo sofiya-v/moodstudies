5e4bf2be1800e0a2990c6f51d4ef966a7a54b573
Sofiya Volobuyeva <sofiyavolobuyeva@gmail.com>
2026-08-22 13:22:24 -0400

    gh workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
+    site: 'https://moodstudies.com',
+
     fonts: [
         {
             provider: fontProviders.google(),
