10d905a53b382821f2e98e33781a231d39a60f60
Sofiya Volobuyeva <sofiyavolobuyeva@gmail.com>
2026-09-11 12:24:34 -0400

    Push remaining assets over SSH

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,21 @@
+
+export default new Map([
+["src/content/sightings/blue-hour-trees.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Fblue-hour-trees.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/blue-window.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Fblue-window.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/east-village-dusk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Feast-village-dusk.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/jordan-blue-hour.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Fjordan-blue-hour.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/lands-end.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Flands-end.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/long-island-city-moon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Flong-island-city-moon.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/los-angeles-dusk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Flos-angeles-dusk.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/moon-shore.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Fmoon-shore.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/purple-dusk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Fpurple-dusk.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/red-blue-mtns.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Fred-blue-mtns.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/red-moon-sea.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Fred-moon-sea.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/rose-dusk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Frose-dusk.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/strip-mall-twilight.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Fstrip-mall-twilight.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/suburban-dusk.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Fsuburban-dusk.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/sunset-blue-hour.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Fsunset-blue-hour.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/train-blue-hour.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Ftrain-blue-hour.mdx&astroContentModuleFlag=true")],
+["src/content/sightings/urban-twilight.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsightings%2Furban-twilight.mdx&astroContentModuleFlag=true")],
+["src/content/studies/001.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fstudies%2F001.mdx&astroContentModuleFlag=true")]]);
+		
---
+++ .astro/fonts.d.ts
@@ -0,0 +1,4 @@
+declare module 'astro:assets' {
+	/** @internal */
+	export type CssVariable = (["--font-syne-mono","--font-silkscreen","--font-vt323"])[number];
+}
